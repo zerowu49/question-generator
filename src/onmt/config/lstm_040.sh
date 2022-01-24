761f74503c2383e328c2cc3b6cb51f213ab8a420
zerowu49 <jonathanputra67@gmail.com>
2022-01-24 21:17:03 +0700

    add: gru_43.sh

--- src/onmt/config/lstm_040.sh
+++ src/onmt/config/lstm_040.sh
@@ -1,7 +1,7 @@
 #================================================== PREPROCESS ==================================================
-onmt_preprocess -train_src 'data/processed/train/squad_id_split0.9_uncased_source.txt' -train_tgt 'data/processed/train/squad_id_split0.9_uncased_target.txt' \
-    -valid_src 'data/processed/val/squad_id_split0.9_uncased_source.txt' -valid_tgt 'data/processed/val/squad_id_split0.9_uncased_target.txt' \
-    -save_data 'data/processed/onmt/squad_id_split0.9_uncased' \
+!onmt_preprocess -train_src '$PROCESS/train/squad_id_split0.9_uncased_source.txt' -train_tgt '$PROCESS/train/squad_id_split0.9_uncased_target.txt' \
+    -valid_src '$PROCESS/val/squad_id_split0.9_uncased_source.txt' -valid_tgt '$PROCESS/val/squad_id_split0.9_uncased_target.txt' \
+    -save_data '$SAVEFILE/squad_id_split0.9_uncased' \
     -overwrite \
     -dynamic_dict \
     -src_vocab_size 50000 \
@@ -10,18 +10,18 @@ onmt_preprocess -train_src 'data/processed/train/squad_id_split0.9_uncased_sourc
     -tgt_seq_length 20
 
 #================================================== EMBEDDING ==================================================
-python src/onmt/embeddings_to_torch.py -emb_file_both 'models/word-embedding/ft_to_gl_300_id.vec' \
-    -dict_file 'data/processed/onmt/squad_id_split0.9_uncased.vocab.pt' \
-    -output_file 'data/processed/onmt/embeddings_uncased'
+!python '$SRCFILE/embeddings_to_torch.py' -emb_file_both '$WE/ft_to_gl_300_id.vec' \
+    -dict_file '$SAVEFILE/squad_id_split0.9_uncased.vocab.pt' \
+    -output_file '$SAVEFILE/embeddings_uncased'
 
 #================================================== TRAIN ==================================================
-onmt_train -data 'data/processed/onmt/squad_id_split0.9_uncased' -save_model 'models/checkpoints/onmt/lstm_040' \
+!onmt_train -data '$SAVEFILE/squad_id_split0.9_uncased' -save_model '$CHKP/onmt/lstm_040' \
     -world_size 1 -gpu_ranks 0 \
     -seed 42 \
     -save_checkpoint_steps 8025 \
     -word_vec_size 300 \
-    -pre_word_vecs_enc 'data/processed/onmt/embeddings_uncased.enc.pt' \
-    -pre_word_vecs_dec 'data/processed/onmt/embeddings_uncased.dec.pt' \
+    -pre_word_vecs_enc '$SAVEFILE/embeddings_uncased.enc.pt' \
+    -pre_word_vecs_dec '$SAVEFILE/embeddings_uncased.dec.pt' \
     -fix_word_vecs_enc \
     -fix_word_vecs_dec \
     -keep_checkpoint 3 \
@@ -41,30 +41,30 @@ onmt_train -data 'data/processed/onmt/squad_id_split0.9_uncased' -save_model 'mo
 
 #================================================== TRANSLATE ==================================================
 #-------------------------------------------------- SQUAD --------------------------------------------------
-onmt_translate -model 'models/checkpoints/onmt/lstm_040_step_16050.pt' \
-    -src 'data/processed/test/squad_id_split0.9_uncased_source.txt' -output 'reports/txts/onmt/lstm_040_step_16050_pred.txt' -replace_unk \
+!onmt_translate -model '$CHKP/onmt/lstm_040_step_16050.pt' \
+    -src '$TESTFILE/squad_id_split0.9_uncased_source.txt' -output '$MODEL/reports/txts/onmt/lstm_040_step_16050_pred.txt' -replace_unk \
     -seed 42 \
     -beam_size 5 \
     -max_length 20
 
 #-------------------------------------------------- TYDIQA --------------------------------------------------
-onmt_translate -model 'models/checkpoints/onmt/lstm_040_step_16050.pt' \
-    -src 'data/processed/test/tydiqa_id_split0.9_uncased_source.txt' -output 'reports/txts/onmt/lstm_040_step_16050_pred_tydiqa.txt' -replace_unk \
+!onmt_translate -model '$CHKP/onmt/lstm_040_step_16050.pt' \
+    -src '$TESTFILE/tydiqa_id_split0.9_uncased_source.txt' -output '$MODEL/reports/txts/onmt/lstm_040_step_16050_pred_tydiqa.txt' -replace_unk \
     -seed 42 \
     -beam_size 5 \
     -max_length 20
 
 #================================================== EVALUATE ==================================================
 #-------------------------------------------------- SQUAD --------------------------------------------------
-python src/onmt/run_evaluation.py \
-    --source_file='data/processed/test/squad_id_split0.9_uncased_source.txt' \
-    --target_file='data/processed/test/squad_id_split0.9_uncased_target.txt' \
-    --prediction_file='reports/txts/onmt/lstm_040_step_16050_pred.txt' \
-    --log_file='reports/txts/onmt/eval_log_lstm_040_step_16050.txt'
+!python '$SRCFILE/run_evaluation.py' \
+    --source_file='$TESTFILE/squad_id_split0.9_uncased_source.txt' \
+    --target_file='$TESTFILE/squad_id_split0.9_uncased_target.txt' \
+    --prediction_file='$MODEL/reports/txts/onmt/lstm_040_step_16050_pred.txt' \
+    --log_file='$MODEL/reports/txts/onmt/eval_log_lstm_040_step_16050.txt'
 
 #-------------------------------------------------- TYDIQA --------------------------------------------------
-python src/onmt/run_evaluation.py \
-    --source_file='data/processed/test/tydiqa_id_split0.9_uncased_source.txt' \
-    --target_file='data/processed/test/tydiqa_id_split0.9_uncased_target.txt' \
-    --prediction_file='reports/txts/onmt/lstm_040_step_16050_pred_tydiqa.txt' \
-    --log_file='reports/txts/onmt/eval_log_lstm_040_step_16050_tydiqa.txt'
+!python '$SRCFILE/run_evaluation.py' \
+    --source_file='$TESTFILE/tydiqa_id_split0.9_uncased_source.txt' \
+    --target_file='$TESTFILE/tydiqa_id_split0.9_uncased_target.txt' \
+    --prediction_file='$MODEL/reports/txts/onmt/lstm_040_step_16050_pred_tydiqa.txt' \
+    --log_file='$MODEL/reports/txts/onmt/eval_log_lstm_040_step_16050_tydiqa.txt'
